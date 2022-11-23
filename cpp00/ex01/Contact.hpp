45c0cd6b56a4f0304f977c9e51b23ec689952505
me <guillaumevial15@gmail.com>
2022-11-23 00:10:03 -0500

    ex01

---
+++ cpp00/ex01/Contact.hpp
@@ -0,0 +1,12 @@
+#ifndef CONTACT_HPP_
+# define CONTACT_HPP_
+
+# include <iostream>
+
+class Contact {
+	public:
+		std::string name;
+		std::string number;
+};
+
+#endif
---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,12 @@
+#ifndef PHONEBOOK_HPP_
+# define PHONEBOOK_HPP_
+
+# include <iostream>
+# include "Contact.hpp"
+
+class PhoneBook {
+	public:
+		Contact contacts[8];
+};
+
+#endif
