5f03da6b7e40e103d5d9659f4a6013d292f76872
me <guillaumevial15@gmail.com>
2022-12-12 20:03:57 -0500

    ClapTrap test update

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -8,7 +8,16 @@ int	main()
 	test.takeDamage(6);
 	test.beRepaired(2);
 
+	std::cout << "\nClapTrap: \n";
 	std::cout << "health: " << test.getHealth() << std::endl;
 	std::cout << "energy:  " << test.getEnergy() << std::endl;
-	std::cout << "damage: " << test.getDamage() << std::endl;
+	std::cout << "damage: " << test.getDamage() << std::endl << std::endl;
+
+	test.takeDamage(6);
+	test.attack("test1");
+	
+	std::cout << "\nClapTrap: \n";
+	std::cout << "health: " << test.getHealth() << std::endl;
+	std::cout << "energy:  " << test.getEnergy() << std::endl;
+	std::cout << "damage: " << test.getDamage() << std::endl << std::endl;
 }
