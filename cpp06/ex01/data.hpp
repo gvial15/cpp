78d96c163e12715081c59d352498a5cf823e6a07
me <guillaumevial15@gmail.com>
2023-01-02 19:15:28 -0500

    ex01

---
+++ cpp06/ex01/data.hpp
@@ -0,0 +1,11 @@
+#ifndef DATA_H
+#define DATA_H
+
+# include <stdint.h>
+# include <iostream>
+
+typedef struct Data {
+	int	num;
+} t_data;
+
+#endif
---
+++ cpp06/ex01/main.cpp
@@ -0,0 +1,18 @@
+#include "data.hpp"
+
+uintptr_t	serialize(Data* ptr) {
+	return ((uintptr_t)(ptr));
+}
+
+Data* deserialize(uintptr_t raw) {
+	return ((Data*)(raw));
+}
+
+int	main() {
+
+	Data	*data = new Data;
+	uintptr_t	uint;
+
+	std::cout << "original pointer: " << data << std::endl;
+	std::cout << "after serialize() / deserialize(): " << deserialize(serialize(data)) << std::endl;
+}
