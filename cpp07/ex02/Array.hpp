6a5a4978d1c573825a0a1f12f99fefde8fc4d53a
me <guillaumevial15@gmail.com>
2023-01-10 23:24:31 -0500

    cpp07 ex02

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,44 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <exception>
+# include <iostream>
+
+template <class C>
+class Array {
+	public:
+		Array() : arr_(new C[0]), size_(0) {}
+		Array(unsigned int n) : arr_(new C[n]), size_(n) {}
+		Array(const Array &og) : arr_(new C[og.size()]), size_(og.size()) {
+			for (int i = 0; i < og.size(); ++i)
+				arr_[i] = og.arr_[i];
+		}
+		Array<C>&	operator=(const Array<C> &og) {
+			size_ = og.size_;
+			for (int i = 0; i < og.size(); ++i)
+				arr_[i] = og.arr_[i];
+			return (*this);
+		}
+		~Array() { delete[] this->arr_; }
+
+	C	&operator[](int index) {
+		if (index >= size_ || index < 0)
+			throw Array::IndexNotInArray();
+		return arr_[index];
+	}
+
+	class IndexNotInArray : public std::exception {
+		public:
+			const char *what() const throw() {
+				return ("index is not inside the array");
+			};
+	};
+
+	unsigned int	size() const { return (size_); }
+
+	private:
+		C				*arr_;
+		unsigned int	size_;
+};
+
+#endif
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,23 @@
+#include "Array.hpp"
+#include <ostream>
+
+int	main() {
+	Array<int>	arr1(5);
+	Array<int>	arr2;
+	arr1[0] = 5;
+	Array<int>	arr3(arr1);
+
+	arr1[0] = 6;
+	arr2 = arr1;
+
+	std::cout << arr1[0] << std::endl;
+	std::cout << arr2[0] << std::endl;
+	std::cout << arr3[0] << std::endl;
+
+	// shows the exception when trying to access an index out of the Array
+	try {
+		std::cout << arr3[6] << std::endl;
+	} catch (std::exception &err) {
+		std::cerr << err.what() << std::endl;
+	}
+}
