e534472d7490a55466f92f59aee7183347f076a6
me <guillaumevial15@gmail.com>
2023-01-17 16:42:57 -0500

    cpp08 not done

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -9,12 +9,11 @@ int	main() {
 	span.addNumber(8);
 	span.addNumber(9);
 	span.addNumber(10);
-	// span is full ! 👇
+	// span is full ! (x2) 👇
 	span.addNumber(11);
 	span.addNumber(12);
 
 	// span2.addNumber(6); // makes span2 unable to take the addNumbers(), is able without it
 	span2.addNumbers(span.vector().begin(), span.vector().end());
-	std::cout << span2.vector().size() << std::endl;
-
+	std::cout << "span2 size: " << span2.vector().size() << std::endl;
 }
