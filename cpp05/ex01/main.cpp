6531736f5e356dc12d1ac55a2b7a40ca72700750
me <guillaumevial15@gmail.com>
2023-01-17 21:42:24 -0500

    cpp05 ex01

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -12,6 +12,8 @@ int	main()
 		std::cerr << err.what() << std::endl;
 	}
 
+	std::cout << "\n";
+
 	// From execute grade to low
 	try {
 		Form form1("form1", 120, 151);
@@ -20,14 +22,20 @@ int	main()
 		std::cerr << err.what() << std::endl;
 	}
 
+	std::cout << "\n";
+
 	// test beSigned and signForm
 	try {
 		Bureaucrat b1 = Bureaucrat("b1", 100);
 		std::cout << b1;
+		std::cout << "\n";
 		Form form1("form1", 100, 120);
 		std::cout << form1;
+		std::cout << "\n";
 		form1.beSigned(b1);
+		std::cout << "\n";
 		std::cout << form1;
+		std::cout << "\n";
 	} catch (std::exception &err) {
 		std::cerr << err.what() << std::endl;
 	}
