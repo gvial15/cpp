9fc0450d9055a6d3c6e65da3d8b33f7611385ec6
me <guillaumevial15@gmail.com>
2023-04-09 13:04:29 -0400

    ex00: put some functions in private

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -9,10 +9,10 @@ class BitcoinExchange {
 		~BitcoinExchange();
 
 	void		display_result();
-	std::string	lower_date(std::string date);
-	std::string	find_closest_date(std::string from_date);
 
 	private:
+		std::string	lower_date(std::string date);
+		std::string	find_closest_date(std::string from_date);
 		std::string						file;
 		std::map<std::string, float>	data_base;
 };
