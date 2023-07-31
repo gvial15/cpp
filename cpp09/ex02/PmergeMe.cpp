fe2bc04576c2d24364f811d0ec94f3de5e976af6
gvial15 <guillaumevial15@gmail.com>
2023-07-30 20:57:11 -0400

    discovered that i had a mistake with the iterator declaration in merge_insert_sort

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -22,74 +22,11 @@ PmergeMe::PmergeMe(char **argv)
 // destructor
 PmergeMe::~PmergeMe() {}
 
-// insert sort
-template<class C>
-void	PmergeMe::insert_sort(C &array, int l, int r)
-{
-	int						tmp;
-	typename C::iterator	i;
-	typename C::iterator	ii;
-	typename C::iterator	prev;
-
-	if (l - 1 == -1)
-		i = array.begin();
-	else
-		i = array.begin() + l - 1;
-	prev = i;
-	while (i != array.begin() + r)
-	{
-		if (i != array.begin() && *i < *prev)
-		{
-			ii = i;
-			while (ii != array.begin() + l - 1 && *i < *--ii);
-			tmp = *i;
-			array.erase(i);
-			array.insert(++ii, tmp);
-		}
-		prev = i;
-		i++;
-	}
-}
-
-// merge sort
-template<class C>
-void	PmergeMe::merge(C &array, int l, int m, int r)
-{
-	int	tmp[r - l + 1], i, j, k;
-
-	i = l;
-	j = m + 1;
-	k = 0;
-	while (i <= m && j <= r)
-	{
-		if (array[i] <= array[j])
-			tmp[k] = array[i++];
-		else
-			tmp[k] = array[j++];
-		k++;
-	}
-	while (i <= m)
-		tmp[k++] = array[i++];
-	while (j <= r)
-		tmp[k++] = array[j++];
-	i = -1;
-	while (++i < k)
-		array[l + i] = tmp[i];
-}
-
 // merge-insert sort
 template<class C>
-void	PmergeMe::merge_insert_sort(C &array, int l, int r, int threshold)
+void	PmergeMe::merge_insert_sort(C &array)
 {
-	if ( l < r)
-	{
-		if (r - l + 1 < threshold)
-			insert_sort(array, l, r);
-		int	m = l + (r - l) / 2;
-		merge_insert_sort(array, l, m, threshold);
-		merge_insert_sort(array, m + 1, r, threshold);
-		merge(array, l, m, r);
-	}
+	typename C::iterator i;
 }
 
 // sort the containers and display the times etc... as required by subject
@@ -118,11 +55,11 @@ void	PmergeMe::sort_and_display_data()
 
 	// sort
 	t_start = std::chrono::high_resolution_clock::now();
-	merge_insert_sort(vec, 0, vec.size() - 1, 3);
+	merge_insert_sort(vec);
 	t_end = std::chrono::high_resolution_clock::now();
 	vec_ms = std::chrono::duration<double, std::milli>(t_end-t_start).count();
 	t_start = std::chrono::high_resolution_clock::now();
-	merge_insert_sort(deque, 0, deque.size() - 1, 3);
+	merge_insert_sort(deque);
 	t_end = std::chrono::high_resolution_clock::now();
 	deque_ms = std::chrono::duration<double, std::milli>(t_end-t_start).count();
 
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -12,7 +12,7 @@ class PmergeMe {
 	template<class C>
 	void	merge(C &array, int l, int m, int r);
 	template<class C>
-	void	merge_insert_sort(C &array, int l, int r, int threshold);
+	void	merge_insert_sort(C &array);
 	void	sort_and_display_data();
 
 	private:
