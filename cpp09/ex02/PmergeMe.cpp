e309ec4946de8bdea3eec882f2b63a11be003c43
gvial15 <guillaumevial15@gmail.com>
2023-08-04 15:13:08 -0400

    merge_insert_sort() fixed

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -22,11 +22,33 @@ PmergeMe::PmergeMe(char **argv)
 // destructor
 PmergeMe::~PmergeMe() {}
 
-// merge-insert sort
-template<class C>
-void	PmergeMe::merge_insert_sort(C &array)
+// insert sort
+template<typename it>
+void	PmergeMe::insert_sort(it start, it end)
 {
-	
+	it	i;
+	it	j;
+
+	for (i = start; i != end; ++i)
+		for (j = i; j != start && *j < *(j - 1); --j)
+			std::iter_swap(j, j - 1);
+}
+
+// merge-insert sort
+template<typename it>
+void	PmergeMe::merge_insert_sort(it start, it end)
+{
+	it	middle;
+
+	if (end - start > 10) 
+	{
+		middle = start + (end - start) / 2;
+		merge_insert_sort(start, middle);
+		merge_insert_sort(middle, end);
+		std::inplace_merge(start, middle, end);
+	}
+	else
+		insert_sort(start, end);
 }
 
 // sort the containers and display the times etc... as required by subject
@@ -52,11 +74,11 @@ void	PmergeMe::sort_and_display_data()
 
 	// sort
 	t_start = std::chrono::high_resolution_clock::now();
-	merge_insert_sort(vec);
+	merge_insert_sort(vec.begin(), vec.end());
 	t_end = std::chrono::high_resolution_clock::now();
 	vec_ms = std::chrono::duration<double, std::milli>(t_end-t_start).count();
 	t_start = std::chrono::high_resolution_clock::now();
-	merge_insert_sort(deque);
+	merge_insert_sort(deque.begin(), deque.end());
 	t_end = std::chrono::high_resolution_clock::now();
 	deque_ms = std::chrono::duration<double, std::milli>(t_end-t_start).count();
 
